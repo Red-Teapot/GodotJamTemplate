04ff9f691126afb342dafcdf28341103f464984b
Nikita Medvedev <some.redteapot@gmail.com>
2022-07-09 16:35:12 +0700

    Initial commit

---
+++ config.example.sh
@@ -0,0 +1,12 @@
+# The name of (or the path to) the Godot executable
+Godot="godot"
+
+# The name of (or the path to) the Butler executable
+# See https://itch.io/docs/butler/
+Butler="butler"
+
+# Username on Itch.io, as it is in the URL
+ItchUsername="example"
+
+# Project name on Itch.io, as it is in the URL
+ItchProjectName="example"
---
+++ export.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -e
+
+cd "$(dirname "$(readlink -f "$0")")"
+
+source config.sh
+
+function exportAndUpload {
+    ExportTemplate="$1"
+    Channel="$2"
+    MainFile="$3"
+
+    echo "Preparing the export directory for $ExportTemplate"
+
+    rm -rf ".export/$Channel"
+    mkdir -p ".export/$Channel"
+
+    echo "Exporting the $ExportTemplate build"
+
+    "$Godot" --no-window --export "$ExportTemplate" ".export/$Channel/$MainFile"
+
+    echo "Uploading the $ExportTemplate build to $ItchUsername/$ItchProjectName:$Channel"
+
+    "$Butler" push ".export/$Channel" "$ItchUsername/$ItchProjectName:$Channel"
+
+    echo "Done exporting the $ExportTemplate build!"
+}
+
+exportAndUpload "Web" "web" "index.html"
+
+exportAndUpload "Windows" "windows" "$ItchProjectName.exe"
